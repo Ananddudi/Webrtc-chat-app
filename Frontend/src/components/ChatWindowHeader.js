1d96a17cb0fe134d04ffc11fa115f8fcc1657a03
anand dudi <dudis.vision@gmail.com>
2024-04-05 13:29:46 +0530

    added online offline status in window

--- Frontend/src/components/ChatWindowHeader.js
+++ Frontend/src/components/ChatWindowHeader.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./chatwindow.css";
 import { IoArrowBack } from "react-icons/io5";
+import { socket } from "../services/socket";
 
 const ChatWindowHeader = ({ item, setSwitched }) => {
   const imageurl = item.profilepic
     ? item.profilepic
     : require("../statics/profileImage.jpg");
+
+  useEffect(() => {
+    // console.log("item", item);
+    const userOnline = (email) => {
+      if (item.email == email) {
+        item.available = "y";
+      }
+    };
+    const userOffline = (email) => {
+      if (item.email == email) {
+        item.available = "n";
+      }
+    };
+    socket.on("online-status", userOnline);
+    socket.on("offline-status", userOffline);
+    return () => {
+      socket.off("online-status", userOnline);
+      socket.off("offline-status", userOffline);
+    };
+  }, []);
   return (
     <div className="chat-list-header">
       <img src={imageurl} alt={item._id} />
